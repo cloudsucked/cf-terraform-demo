5183da410eaf9e13d1bd5aa97ace049a590170f2
Nico van Niekerk <nico@cloudflare.com>
2024-01-30 21:20:00 +1100

    adds one plan is ENT for ALW

--- api_shield.tf
+++ api_shield.tf
@@ -21,7 +21,7 @@ resource "cloudflare_api_shield_schema" "petstore_schema" {
 resource "cloudflare_api_shield_schema_validation_settings" "petstore_schema" {
   zone_id                               = var.cloudflare_zone_id
   validation_default_mitigation_action  = "log"
-  validation_override_mitigation_action = "disable_override"
+  validation_override_mitigation_action = "none"
 }
 
 resource "cloudflare_api_shield_operation" "httpbin_block" {
