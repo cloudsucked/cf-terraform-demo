7fb976d1a802aafc6b360cd058463e08040726e2
Nico van Niekerk <nico@cloudflare.com>
2024-03-31 15:41:16 +1100

    change order of expression

--- waf_account_level.tf
+++ waf_account_level.tf
@@ -118,7 +118,7 @@ resource "cloudflare_ruleset" "account_level_managed_waf" {
         }
       }
     }
-    expression  = "((cf.zone.plan eq \"ENT\") and (http.host contains \"petstore.\"))"
+    expression  = "((http.host contains \"petstore.\") and (cf.zone.plan eq \"ENT\"))"
     description = "OWASP Ruleset"
     enabled     = true
   }
