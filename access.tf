09c60fdf05a6afec95db40a63bca5a02421d06d4
Nico van Niekerk <nico@cloudflare.com>
2024-05-27 09:36:36 +1000

    Access Policy changes

--- access.tf
+++ access.tf
@@ -6,15 +6,16 @@ resource "cloudflare_access_application" "httpbin_application" {
   type                      = "self_hosted"
   session_duration          = "8h"
   auto_redirect_to_identity = false
+  policies                  = [cloudflare_access_policy.httpbin_policy.id]
 }
 
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/access_policy
 resource "cloudflare_access_policy" "httpbin_policy" {
-  application_id = cloudflare_access_application.httpbin_application.id
-  zone_id        = var.cloudflare_zone_id
-  name           = "Test Users"
-  precedence     = "1"
-  decision       = "allow"
+  # application_id = cloudflare_access_application.httpbin_application.id
+  zone_id = var.cloudflare_zone_id
+  name    = "Test Users"
+  # precedence = "1"
+  decision = "allow"
 
   include {
     # group = ["uuid-of-group"]
@@ -30,14 +31,15 @@ resource "cloudflare_access_application" "petstore_application" {
   type                      = "self_hosted"
   session_duration          = "8h"
   auto_redirect_to_identity = false
+  # policies                  = [cloudflare_access_policy.petstore_policy.id] # DEPRECATION: uncomment
 }
 
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/access_policy
 resource "cloudflare_access_policy" "petstore_policy" {
-  application_id = cloudflare_access_application.petstore_application.id
+  application_id = cloudflare_access_application.petstore_application.id # DEPRECATION: comment out
   zone_id        = var.cloudflare_zone_id
   name           = "Test Users"
-  precedence     = "1"
+  precedence     = "1" # DEPRECATION: comment out
   decision       = "allow"
 
   include {
